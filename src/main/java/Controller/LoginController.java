7b14e02c5edbe4699eebf6da8bda86222318044d
Juan Camilo Cuenca Sepulveda <camilocuenca1810@gmail.com>
2024-04-07 15:36:58 -0500

    ADD: Crear métodos para verificar los datos de los usuarios.
    En este commit se agregaron métodos en la clase DataUtils.java para verificar los datos de los usuarios.
    
    -Método para verificar los datos proporsionados por el usuario en el login para determinar si  esta registrado o no.
    -Método para verificar que tipo de administrador es el usuario.

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.utils.DataUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -25,7 +26,15 @@ public class LoginController {
 
     @FXML
     void ingresar(ActionEvent event) {
-
+        String rutaAdminFactura = "src/main/resources/CSVFiles/Administradores.txt";
+        boolean datos =  DataUtils.verificarDatosUsuarios(txtNombre.getText(),txtContrasena.getText(),rutaAdminFactura);
+        if(datos){
+            System.out.println("Funciono");
+            System.out.println(DataUtils.verificarTipoadmin());
+        }
+        else {
+            System.out.println("no es correto");
+        }
     }
 
     @FXML
