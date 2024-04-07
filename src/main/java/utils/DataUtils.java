c50e423487ecd2ffe1b1bebea5c5fe8e869ab216
Juan Camilo Cuenca Sepulveda <camilocuenca1810@gmail.com>
2024-04-07 00:55:56 -0500

    ADD: Crear función para cargar archivos CSV desde el explorador de archivos
    En este commit se añade una nueva función que permite cargar archivos CSV desde el explorador de archivos. Se realizaron los siguientes cambios:
    
    - Se implementó la función UploadCSVfile para abrir el explorador de archivos y cargar archivos CSV.
    - Se añadió un filtro para mostrar solo archivos de texto en el explorador.
    - Los archivos CSV cargados se guardan automáticamente en la carpeta "CSVFiles" del proyecto.
    
    Este commit agrega funcionalidad importante para la aplicación, permitiendo a los usuarios cargar archivos CSV de manera fácil y guardarlos en una ubicación específica para su posterior procesamiento.

--- src/main/java/utils/DataUtils.java
+++ src/main/java/utils/DataUtils.java
@@ -10,6 +10,38 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 public class DataUtils {
+    /**
+     * Esta función abre un cuadro de diálogo para que el usuario seleccione un archivo CSV.
+     * Una vez seleccionado el archivo, lo copia al directorio "src/main/resources/CSVFiles" dentro del proyecto.
+     * Si el directorio de destino no existe, se crea automáticamente.
+     */
+    public static void UploadCSVfile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Archivos de texto", "*.txt", "*.csv")); // Filtro para archivos de texto
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
 
+        if (selectedFile != null) {
+            try {
+                System.out.println("Ruta del archivo seleccionado: " + selectedFile.getAbsolutePath());
+
+                // Verificar si el directorio de destino existe, si no, crearlo
+                Path destinationDirectory = Path.of("src/main/resources/CSVFiles");
+                if (!Files.exists(destinationDirectory)) {
+                    Files.createDirectories(destinationDirectory);
+                }
+
+                // Copiar el archivo seleccionado al directorio de destino
+                Path destinationPath = destinationDirectory.resolve(selectedFile.getName());
+                Files.copy(selectedFile.toPath(), destinationPath, StandardCopyOption.REPLACE_EXISTING);
+
+                System.out.println("Archivo guardado en: " + destinationPath);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("No se seleccionó ningún archivo.");
+        }
+    }
 
 }
