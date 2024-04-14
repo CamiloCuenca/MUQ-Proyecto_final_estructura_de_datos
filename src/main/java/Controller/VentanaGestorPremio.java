534027b09005842120e543cd4d55ed8a9eed6491
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2024-04-13 21:03:27 -0500

    metodo para leer

--- src/main/java/Controller/VentanaGestorPremio.java
+++ src/main/java/Controller/VentanaGestorPremio.java
@@ -1,4 +1,76 @@
 package Controller;
 
-public class VentanaGestorPremio {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VentanaGestorPremio implements Initializable {
+    @FXML
+    private Button ButtonActualizar;
+
+    @FXML
+    private Button ButtonBuscar;
+
+    @FXML
+    private Button ButtonEscogerPremio;
+
+    @FXML
+    private Button ButtonSalir;
+
+    @FXML
+    private TableColumn<?, ?> colCantidad;
+
+    @FXML
+    private TableColumn<?, ?> colCliente;
+
+    @FXML
+    private TableColumn<?, ?> colFactura;
+
+    @FXML
+    private TableColumn<?, ?> colNombre;
+
+    @FXML
+    private TableColumn<?, ?> colPremio;
+
+    @FXML
+    private TableColumn<?, ?> colProducto;
+
+    @FXML
+    private TableColumn<?, ?> colTioo;
+
+    @FXML
+    private TableView<?> tblPremiosPersonas;
+
+    @FXML
+    private TableView<?> tblProductos;
+
+    @FXML
+    private TextField txtBuscarCliente;
+
+    @FXML
+    void OnActualizar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnBuscar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnEscoger(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/main/java/Model/objetos/AdminPremio.java
+++ src/main/java/Model/objetos/AdminPremio.java
@@ -1,7 +1,12 @@
 package Model.objetos;
 
 import Model.enums.Genero;
+import Model.enums.Paises;
+import Model.enums.TipoProducto;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class AdminPremio extends Usuario{
@@ -20,7 +25,6 @@ public class AdminPremio extends Usuario{
         int prioridad = 0;
         int pais=1;
 
-
         if(factura.getCliente().getEdad()>60){
             prioridad=3;
 
@@ -32,6 +36,82 @@ public class AdminPremio extends Usuario{
         PersonaPremio premio=new PersonaPremio(prioridad,pais,secuencia,factura.getCliente());
 
         return premio;
+    }
+
+    public void lectorTXT (){
+
+        String filePath = "MUQ-Proyecto_final_estructura_de_datos\\src\\main\\resources\\CSVFiles\\Facturas"; // Ruta al archivo de texto
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+
+            while ((line = bufferedReader.readLine()) != null) {
+                // Dividir la línea en partes usando el delimitador ";"
+                String[] parts = line.split(";");
+
+                // Imprimir cada parte
+                for (int i=0; i<parts.length; i++){
+                    String ID="";
+                    String IDFactura="";
+                    String productos="";
+                    TipoProducto tipoProducto=TipoProducto.ALIMENTO;
+                    double valorTotal=0;
+                    String DIA="0";
+                    String MES="0";
+                    String ANO="0";
+                    Cliente cliente;
+
+                    //Datos cliente
+                    String nombre="";
+                    int edad=0;
+                    Genero sexo=Genero.OTRO;
+                    Paises pais=Paises.COLOMBIA;
+                    String ciudad="";
+                    if(i==0){
+                        IDFactura=parts[0];
+                        //51055;718674528;Patricia;78;MUJER;COLOMBIA;Pereira;Refrigerador;ELECTRODOMESTICO;1100000;17;06;2023
+                    }else if(i==1){
+                        ID=parts[1];
+                    }else if(i==2){
+                        nombre=parts[2];
+                    }else if(i==3){
+                        edad=Integer.parseInt(parts[3]);
+                    }else if(i==4){
+                        if(parts[4].equals("COLOMBIA")){
+                            pais=Paises.COLOMBIA;
+                        }else if(parts[4].equals("INDIA")){
+                            pais=Paises.INDIA;
+
+                        }else if(parts[4].equals("ARGENTINA")) {
+                            pais = Paises.ARGENTINA;
+                        }
+                    }else if(i==5){
+                        ciudad=parts[5];
+                    }else if(i==7){
+
+
+                    }else if(i==8){
+                        if(parts[7].equals("ALIMENTO")){
+                            tipoProducto=TipoProducto.ALIMENTO;
+                        }else if(parts[7].equals("COSMETICO")){
+                            tipoProducto=TipoProducto.COSMETICO;
+                        }else if(parts[7].equals("ELECTRODOMESTICO")){
+                            tipoProducto=TipoProducto.ELECTRODOMESTICO;
+                        }else if(parts[7].equals("TECNOLOGIA")){
+                            tipoProducto=TipoProducto.TECNOLOGIA;
+                        }
+
+
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+
+
+
     }
 
     public void generarAtributosPremio (){
